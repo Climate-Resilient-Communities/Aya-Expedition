87237ad04e5f17094d09f743f698891e5d41df73
lutic1 <64680897+lutic1@users.noreply.github.com>
2024-08-22 00:32:30 -0400

    Update Multilingual_Chatbot_Pinecone.py

--- Multilingual_Chatbot_Pinecone.py
+++ Multilingual_Chatbot_Pinecone.py
@@ -28,8 +28,8 @@ import streamlit as st
 COHERE_API_KEY = st.secrets["api_keys"]["cohere_api_key"]
 LANGCHAIN_ENDPOINT  = st.secrets["api_keys"]["langchain_endpoint"]
 LANGCHAIN_API_KEY = st.secrets["api_keys"]["langchain_api_key"]
-TAVILY_API_KEY = st.secrets["api_keys"]["tavily_api_key"]
-tavily_api_key = st.secrets["api_keys"]["TAVILY_API_KEY"]
+#TAVILY_API_KEY = st.secrets["api_keys"]["tavily_api_key"]
+#tavily_api_key = st.secrets["api_keys"]["TAVILY_API_KEY"]
 TAVILY_API_KEY = st.secrets["tavily_api_key"]
 pinecone_api_key = st.secrets["api_keys"]["PINECONE_API_KEY"]
 
