4a90e98d3fb76fc0bb04bedb14cdb03c4e7e1f44
lutic1 <64680897+lutic1@users.noreply.github.com>
2024-08-22 00:25:59 -0400

    Update Multilingual_Chatbot_Pinecone.py

--- Multilingual_Chatbot_Pinecone.py
+++ Multilingual_Chatbot_Pinecone.py
@@ -30,7 +30,7 @@ LANGCHAIN_ENDPOINT  = st.secrets["api_keys"]["langchain_endpoint"]
 LANGCHAIN_API_KEY = st.secrets["api_keys"]["langchain_api_key"]
 TAVILY_API_KEY = st.secrets["api_keys"]["tavily_api_key"]
 tavily_api_key = st.secrets["api_keys"]["TAVILY_API_KEY"]
-["TAVILY_API_KEY"] = st.secrets["api_keys"]["tavily_api_key"]
+TAVILY_API_KEY = st.secrets["tavily_api_key"]
 pinecone_api_key = st.secrets["api_keys"]["PINECONE_API_KEY"]
 
 # Accessing settings
